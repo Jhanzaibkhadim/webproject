47bd4319ad235aa3d4998d280d4ed3825a297cbf
Jhanzeb Khadim <dsu.cs.1.13.012@gmail.com>
2018-11-18 23:26:28 +0500

    textarea and dropdown issue resolved

--- src/form/buyerForm.js
+++ src/form/buyerForm.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import './buyerform.css'
-import swal from 'sweetalert2'
 export default class Buyerform extends Component {
 
 
 
     constructor() {
         super()
-        // this.demo();
+       
         this.state = {
 
             shouldHide:true,
@@ -15,15 +14,6 @@ export default class Buyerform extends Component {
         }
     }
 
-    // demo(){
-    //     console.log(this.state.value)
-        
-    //     if(this.state.value !== ''){
-    //         this.state.shouldHide = false;
-    //     }else{
-    //         this.state.shouldHide = true;
-    //     }
-    // }
     demo = (event) => {
 
         this.setState({
@@ -37,37 +27,6 @@ export default class Buyerform extends Component {
             this.state.shouldHide = true;
         }
     }
-
-    // demo() {
-    //     const {value: fruit} =  swal({
-    //         title: 'Are you the Buyer or Seller?',
-    //         input: 'select',
-    //         inputOptions: {
-    //           'buyer': 'Buyer',
-    //           'seller': 'Seller'
-    //         },
-    //         inputPlaceholder: 'Please Select Here',
-    //         showCancelButton: true,
-    //         backdrop:false,
-    //         // onClose(){
-    //         //     this.state.shouldHide = false;
-    //         //     alert(this.state.shouldHide);
-
-    //         // },
-    //         inputValidator: (value) => {
-    //           return new Promise((resolve) => {
-    //             if (value !== '') {
-    //                 this.state.shouldHide = false
-    //                 // alert(this.state.shouldHide)
-    //               resolve()
-                   
-    //             } else {
-    //               resolve('You need to select')
-    //             }
-    //           })
-    //         }
-    //       })
-    // }
 
 
     render() {
@@ -118,7 +77,6 @@ export default class Buyerform extends Component {
                                     <label>Amount type</label>
                                     <select className="selectclass" name="coins">
                                         <option value="bitcoin">BTC</option>
-                                        {/* <option value="saab">Saab</option> */}
                                        
                                     </select>
                                 </p>
@@ -131,13 +89,7 @@ export default class Buyerform extends Component {
                                         <option value="saab">Custom Split</option>
                                     </select>
                                 </p>
-                                <p>
-                                    <label>Goods or Service?</label>
-                                    <select className="selectclass" name="coins">
-                                        <option value="bitcoin">Goods</option>
-                                       
-                                    </select>
-                                </p>
+                                
                                 <p className="full">
                                     <label>Seller's BTC Address</label>
                                     <textarea name="address" placeholder="Enter address" rows="2"></textarea>
@@ -158,60 +110,3 @@ export default class Buyerform extends Component {
     }
 }
 
-// var Results = React.createClass({
-//     render: function() {
-//         return (
-//             <div>
-//                 {/* <div>
-//                     <select>
-//                         <option >Buyer</option>
-//                         <option >seller</option>
-
-//                     </select>
-//                 </div> */}
-//                 <div className="container" >
-//                     <h1 className="brand"><span>Coin</span> Grace</h1>
-//                     <div className="wrapper animated bounceInLeft">
-//                         <div className="company-info">
-//                             <h3 onC>How it works ?</h3>
-//                             <ul>
-//                                 <li><i ></i> </li>
-//                                 <li><i ></i> </li>
-//                                 <li><i ></i> </li>
-//                             </ul>
-//                         </div>
-//                         <div className="contact">
-//                             <h3>Welcome to Escrow</h3>
-
-//                             <form action="send">
-//                                 <p>
-//                                     <label>Name</label>
-//                                     <input type="text" name="name" />
-//                                 </p>
-//                                 <p>
-//                                     <label>Company</label>
-//                                     <input type="text" name="company" />
-//                                 </p>
-//                                 <p>
-//                                     <label>Email Address</label>
-//                                     <input type="email" name="email" />
-//                                 </p>
-//                                 <p>
-//                                     <label>Phone Number</label>
-//                                     <input type="text" name="phone" />
-//                                 </p>
-//                                 <p className="full">
-//                                     <label>Message</label>
-//                                     <textarea name="message" rows="5"></textarea>
-//                                 </p>
-//                                 <p className="full">
-//                                     <button type="submit">Submit</button>
-//                                 </p>
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// });
